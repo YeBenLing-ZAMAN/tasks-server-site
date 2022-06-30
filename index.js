4f3f1f567005dbc5d87baeee8e111fa9d4af7603
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-06-30 22:58:27 +0600

    jwt token getting and verifyJWT route added

--- index.js
+++ index.js
@@ -17,6 +17,26 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.phion.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+/* jwt verifcation */
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    // console.log(authHeader);
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+  
+  
+
 
 async function run() {
     try {
@@ -26,7 +46,7 @@ async function run() {
 
 
         /* routes */
-        app.get('/billing_list', async (req, res) => {
+        app.get('/billing_list',verifyJWT, async (req, res) => {
             const bills = await billingCollection.find().toArray();
             res.send(bills);
         })
@@ -37,7 +57,7 @@ async function run() {
             const bill = await billingCollection.findOne(filter);
             // console.log(bill);
             res.send(bill);
-          })
+        })
 
         app.post('/add_billing', async (req, res) => {
             const bill = req.body;
@@ -51,16 +71,16 @@ async function run() {
             // console.log(updateBillInfo);
             const filter = { _id: ObjectId(id) };
             const updateDoc = {
-              $set: {
-                full_name: updateBillInfo.name,
-                email:updateBillInfo.email,
-                paid_amount:updateBillInfo.amount,
-                phone:updateBillInfo.phone
-              }
+                $set: {
+                    full_name: updateBillInfo.name,
+                    email: updateBillInfo.email,
+                    paid_amount: updateBillInfo.amount,
+                    phone: updateBillInfo.phone
+                }
             }
             const result = await billingCollection.updateOne(filter, updateDoc);
             res.send(result);
-          })
+        })
 
         app.delete('/deletebilling/:id', async (req, res) => {
             const id = req.params.id;
@@ -90,6 +110,20 @@ async function run() {
         //     const result = await userCollection.updateOne(filter, updateDoc);
         //     res.send(result);
         // })
+
+        /* user token generated */
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
+        })
 
     } finally {
         // Ensures that the client will close when you finish/error
