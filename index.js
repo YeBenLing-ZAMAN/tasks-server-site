7facf6fa3530346a771dd29c47a4b164851fa651
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-06-30 12:40:19 +0600

    get request is handled

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -12,16 +13,32 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.phion.mongodb.net/?retryWrites=true&w=majority";
+/* basic connect with mongodb and create a client */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.phion.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("jobs_tasks").collection("bill");
-  // perform actions on the collection object
-  console.log('db connected');
-  client.close();
-});
 
 
+async function run() {
+    try {
+        await client.connect();
+        const billingCollection = client.db("jobs_tasks").collection("billing_info");
+
+
+        /* routes */
+        app.get('/billing_list', async (req, res) => {
+            const bills = await billingCollection.find().toArray();
+            res.send(bills);
+        })
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello World! this is zaman ! connected sucessfully')
+})
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
