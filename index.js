5afb51dc5fd7c72aa32a08847e195573217e9aae
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-06-30 14:58:58 +0600

    add bill post method is developed

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ async function run() {
             res.send(bills);
         })
 
+        app.post('/add_billing', async(req, res)=>{
+            const bill = req.body;
+            const result = await billingCollection.insertOne(bill);
+            res.send(result);
+        })
+
     } finally {
         // Ensures that the client will close when you finish/error
         //   await client.close();
